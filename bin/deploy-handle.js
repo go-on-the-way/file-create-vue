1f74756f7397838fadddebe87376d1f5e8f407fd
wlei <wangleiontheway@163.com>
2021-09-24 19:02:49 +0800

    代码优化

--- bin/deploy-handle.js
+++ bin/deploy-handle.js
@@ -5,34 +5,60 @@ const { spawn } = require('child_process');
 let { PromiseProcessHandler } = require('../private/util')
 
 async function executePatchCommand(opts){
-    let lqProcess = ora(`
-        此次操作会依次执行如下命令请耐心等待:\n\r
-        1、git checkout master\n\r
-        2、git pull\n\r
-        3、git merge --no-ff origin/release/v${opts.oldVersion}\n\r
-        4、git pull\n\r
-        5、git push\n\r
-        6、git tag -a v${opts.oldVersion} -m ${opts.annotation}\n\r
-        7、git push origin v${opts.oldVersion}\n\r
-        8、git branch release/v${opts.newVersion}\n\r
-        9、git push origin release/v${opts.newVersion}:release/v${opts.newVersion}
-    `)
-    try {
-        lqProcess.start()
-        await PromiseProcessHandler(spawn('git', ['checkout','master']))
-        await PromiseProcessHandler(spawn('git', ['pull']))
-        await PromiseProcessHandler(spawn('git', ['merge','--no-ff',`origin/release/v${opts.oldVersion}`]))
-        await PromiseProcessHandler(spawn('git', ['pull']))
-        await PromiseProcessHandler(spawn('git', ['push']))
-        await PromiseProcessHandler(spawn('git', ['tag','-a',`v${opts.oldVersion}`,'-m',opts.annotation]))
-        await PromiseProcessHandler(spawn('git', ['push','origin',`v${opts.oldVersion}`]))
-        await PromiseProcessHandler(spawn('git', ['branch',`release/v${opts.newVersion}`]))
-        await PromiseProcessHandler(spawn('git', ['push','origin',`release/v${opts.newVersion}:release/v${opts.newVersion}`]))
-        lqProcess.succeed()
-    } catch (error) {
-        console.log(error)
-        lqProcess.fail()
-    }
+    let spinner = ora(`处理中....
+        
+        7、\n\r
+        8、\n\r
+        9、
+    `).start()
+    
+    
+    spinner.text = 'git checkout master'
+    await PromiseProcessHandler(spawn('git', ['checkout','master'])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git pull`
+    await PromiseProcessHandler(spawn('git', ['pull'])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git merge --no-ff origin/release/v${opts.oldVersion}`
+    await PromiseProcessHandler(spawn('git', ['merge','--no-ff',`origin/release/v${opts.oldVersion}`])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git pull`
+    await PromiseProcessHandler(spawn('git', ['pull'])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git push`
+    await PromiseProcessHandler(spawn('git', ['push'])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git tag -a v${opts.oldVersion} -m ${opts.annotation}`
+    await PromiseProcessHandler(spawn('git', ['tag','-a',`v${opts.oldVersion}`,'-m',opts.annotation])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git push origin v${opts.oldVersion}`
+    await PromiseProcessHandler(spawn('git', ['push','origin',`v${opts.oldVersion}`])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git branch release/v${opts.newVersion}`
+    await PromiseProcessHandler(spawn('git', ['branch',`release/v${opts.newVersion}`])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+
+    spinner.text = `git push origin release/v${opts.newVersion}:release/v${opts.newVersion}`
+    await PromiseProcessHandler(spawn('git', ['push','origin',`release/v${opts.newVersion}:release/v${opts.newVersion}`])).catch(()=>{
+        spinner.fail(spinner.text)
+    })
+    spinner.succeed()
+    
 }
 
 program.parse(process.argv)
